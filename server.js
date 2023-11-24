2d3378bcec16d43ab697961045f7e9e4025db49a
0xAnon <utkarshshail005@protonmail.com>
2023-11-24 12:28:22 +0530

    Webhook fixed

--- server.js
+++ server.js
@@ -45,7 +45,6 @@ db.once("open", () => {
   console.log("Connected to MongoDB");
 });
 
-app.use(bodyParser.json());
 app.use(cors());
 
 gpuMarketplaceContractWS.on("MachineListed", (_machineId, _name) => {
@@ -345,15 +344,17 @@ app.post("/gPBuyWithStripe", async(req, res) => {
   }
 })
 
-app.post('/stripeWebhook', bodyParser.raw({type: 'application/json'}), (req, res) => {
+app.post('/stripeWebhook', express.raw({type: 'application/json'}), (req, res) => {
 
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET
   const sig = req.headers['stripe-signature'];
+  const payload = req.body
 
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
+    event = stripe.webhooks.constructEvent(payload, sig, webhookSecret);
+    console.log(event.type)
   } catch (err) {
     console.error('Webhook Error:', err.message);
     return res.status(400).send(`Webhook Error: ${err.message}`);
@@ -361,7 +362,7 @@ app.post('/stripeWebhook', bodyParser.raw({type: 'application/json'}), (req, res
 
   switch (event.type) {
 
-    case 'checkout.session.completed':
+    case 'payment_intent.succeeded':
       const session = event.data.object;
       console.log('Payment successful. Session ID:', session.id);
       break;
