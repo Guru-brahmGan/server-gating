7bb036b3e3ef36c16703b9460adc21a79388a35c
0xAnon <utkarshshail005@protonmail.com>
2023-12-15 16:44:45 +0530

    Removed unnecessary package

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const axios = require("axios");
